4a2004344d2928b1a3866a1f9441c633b9a602b5
MaxiIvan <85696496+maxiivandlf@users.noreply.github.com>
2022-04-17 23:14:46 -0300

    DarkMode

--- javascript/index.js
+++ javascript/index.js
@@ -12,3 +12,17 @@ btn.addEventListener('click', function (e) {
     nav.classList.remove('navigation');
   }
 });
+
+// dark and ligth
+var icon = document.getElementById('icon');
+
+icon.onclick = () => {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    icon.src = './src/icon/moon.png';
+    icon.title = 'Modo dark';
+  } else {
+    icon.src = './src/icon/sun.png';
+    icon.title = 'Modo Ligth';
+  }
+};
